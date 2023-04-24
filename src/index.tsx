d29e302f20da593ed21548b4b9198fcf4da8db8b
Sam Miller <sam@creative2.com>
2023-04-24 16:41:29 -0400

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,34 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, Clipboard } from "@raycast/api";
 
 type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
+  width_1: number;
+  width_2: number;
+  height_1: number;
+  height_2: number;
 };
 
 export default function Command() {
   function handleSubmit(values: Values) {
     console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
+
+    //if width_2 is not empty
+    if (values.width_2 && !values.height_2) {
+        // calculate height_2 based on width_1 and height_1 aspect ratio
+        values.height_2 = Math.round((values.width_2 * values.height_1) / values.width_1);
+        console.log(values.height_2);
+        Clipboard.copy(values.height_2);
+        showToast({ title: "Calculated", message: "Height 2: " + values.height_2 });
+    }
+
+    if (values.height_2 && !values.width_2) {
+        // calculate width_2 based on width_1 and height_1 aspect ratio
+        values.width_2 = Math.round((values.height_2 * values.width_1) / values.height_1);
+        console.log(values.width_2);
+        Clipboard.copy(values.width_2);
+        showToast({ title: "Calculated", message: "Width 2: " + values.width_2 });
+    }
+
+    // showToast({ title: "Submitted form", message: "See logs for submitted values" });
   }
 
   return (
@@ -23,18 +39,12 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
+      <Form.Description text="Calculate yourself some aspect ratio values." />
+      <Form.TextField id="width_1" title="Width 1" placeholder="1920" />
+      <Form.TextField id="height_1" title="Height 1" placeholder="1080" />
       <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
-      </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
+      <Form.TextField id="width_2" title="Width 2" placeholder="Enter value" />
+      <Form.TextField id="height_2" title="Height 2" placeholder="Enter value" />
     </Form>
   );
 }
